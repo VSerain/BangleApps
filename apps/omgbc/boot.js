058d726fbf8600ad43816ce6974e6b5517075e7e
Victor Serain <vserain@leadformance.com>
2021-12-23 15:35:21 +0100

    feat: update apps for add settings

--- apps/omgbc/boot.js
+++ apps/omgbc/boot.js
@@ -117,3 +117,144 @@
         else dispatchEvent(event.t, event);
     };
 })();
+
+
+/**
+ * 
+ * function drawMessage(message) {
+  g.clearRect(0,g.getHeight() / 2 - g.getHeight() / 3, g.getWidth(), g.getHeight() / 2 + g.getHeight() / 4);
+  // Draw Message rect
+  g.setColor(1,1,1);
+  g.fillRect(
+    0,
+    g.getHeight() / 2 - g.getHeight() / 3,
+    g.getWidth(),
+     g.getHeight() / 2 + g.getHeight() / 4
+  );
+
+  drawHeader(message);
+  drawMessageTxt(message);
+  drawActionsButtons();
+}
+
+function drawHeader(message) {
+  g.setFont("6x8", 2.5).setFontAlign(-1,-1);
+  g.setColor(0,0,0);
+
+  g.drawString(
+    message.sender.trim(),
+    10,
+    g.getHeight() / 2 - g.getHeight() / 3 + 10
+  );
+  if (message.src) {
+    g.setFont("6x8", 1).setFontAlign(-1, 0);
+    g.drawString(
+      message.src.trim(),
+      g.getWidth() - g.stringWidth(message.src.trim()) - 5,
+      g.getHeight() / 2 - g.getHeight() / 3 + 7
+    );
+  }
+}
+
+function drawMessageTxt(message) {
+  const width = g.getWidth() - 10;// Available width
+  const height = (g.getHeight() / 2 + g.getHeight() / 4) - (g.getHeight() / 2 - g.getHeight() / 3) - 50;// Available height
+  const lineHeight = 6 * 2;
+  const nbLine = Math.ceil(height / (lineHeight+4));
+  g.setFont("6x8", 2).setFontAlign(-1,-1);
+
+  const words = message.body.replace("\n", "").split(" ");
+  var currentWords = 0;
+  for (var i = 0; i < nbLine; i++) {
+    if (currentWords >= words.length) continue;
+    var line = "";
+    for(let x = currentWords; x < words.length; x++) {
+      var word = words[x];
+      if (g.stringWidth(line + " " + word) > width) {
+        break;
+      } else {
+        line += " " + word;
+        currentWords = x;
+      }
+    }
+    currentWords++;
+     g.drawString(
+      line,
+      5,
+      g.getHeight() / 2 - g.getHeight() / 3 + 40 + ((lineHeight + 4) * i)
+    );
+  }
+}
+
+function drawActionsButtons() {
+  g.setFont("6x8", 2.5).setFontAlign(0,0);
+
+  // Draw Button actions
+  g.setColor(0.278, 0.886, 0.235);
+  g.fillCircle(
+    g.getWidth() / 2 - g.getWidth() / 3.8,
+    g.getHeight() / 2 + g.getHeight() / 4 + 20,
+    35
+  );
+  g.setColor(0,0,0);
+  g.drawString(
+    "Seen",
+    g.getWidth() / 2 - g.getWidth() / 3.8,
+    g.getHeight() / 2 + g.getHeight() / 4 +20
+  );
+
+  g.setColor(0.886, 0.235, 0.466);
+  g.fillCircle(
+    g.getWidth() / 2 + g.getWidth() / 3.8,
+    g.getHeight() / 2 + g.getHeight() / 4 + 20,
+    35
+  );
+
+  g.setColor(0,0,0);
+  g.drawString(
+    "Not\nseen",
+    g.getWidth() / 2 + g.getWidth() / 3.8,
+    g.getHeight() / 2 + g.getHeight() / 4 +20
+  );
+}
+
+
+
+function onMessage(message) {
+  const oldMode = Bangle.getLCDMode();
+  Bangle.setLCDMode("direct");
+  Bangle.setLCDPower(1);
+  //g.setLCDOffset(g.getHeight());
+  drawMessage(message);
+  Bangle.buzz();
+  setTimeout( _ => Bangle.buzz(), 1000);
+  let isClose = false;
+
+  function close() {
+    isClose = true;
+    g.clearRect(0,g.getHeight() / 2 - g.getHeight() / 3, g.getWidth(), g.getHeight());
+    Bangle.setLCDMode(oldMode);
+    //g.setLCDOffset(0);
+  }
+
+  setWatch(() => {
+    if (!global.GadgetBridge || isClose) return;
+    global.GadgetBridge.messageResponse(message, true);
+    close();
+  }, BTN4, {edge:"both", repeat: false});
+
+  setWatch(() => {
+    if (!global.GadgetBridge || isClose) return;
+    close();
+    global.GadgetBridge.messageResponse(message, false);
+  }, BTN5, {edge:"both", repeat: false});
+}
+
+onMessage({
+  body: "slkdfjsljdf, sfsdfsdf",
+  sender: "Victor",
+  id: 23
+});
+
+global.GadgetBridge.onEvent("notify", (message) => onMessage(message));
+ */
